a295a39457534dc732791bd45514982bc3a1890f
mohamedalichelbi <mohamedalichelbi@icloud.com>
2017-08-09 20:14:20 +0300

    Add files via upload

---
+++ plane/RECEIVER/sketch_aug08b/sketch_aug08b.ino
@@ -0,0 +1,34 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+RF24 radio(7, 8); // CNS, CE
+const byte addresses[][6] = {"00001", "00002", "00003"};
+Servo myServo;
+void setup() {
+  myServo.attach(3);
+  myServo1.attach(5);
+  myServo2.attach(6);
+  radio.begin();
+  radio.openReadingPipe(addresses[1]); // 00001
+  radio.openReadingPipe(2, addresses[2]); // 00002
+  radio.openReadingPipe(3, addresses[3]); // 00003
+  radio.setPALevel(RF24_PA_MIN);
+}
+void loop() {
+  delay(5);
+  radio.startListening();
+  if ( radio.available()) {
+    while (radio.available()) {
+      int angleV = 0;
+      radio.read(&angleV, sizeof(angleV));
+      myServo.write(angleV);
+          int angleV1 = 0;
+          radio.read(&angleV1, sizeof(angleV1));
+          myServo.write(angleV1);
+              int angleV2 = 0;
+              radio.read(&angleV2, sizeof(angleV2));
+              myServo.write(angleV2);
+    }
+  }
+}
---
+++ plane/TRANSMITTER/sketch_aug08a/sketch_aug08a.ino
@@ -0,0 +1,26 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CNS, CE
+const byte addresses[][6] = {"00001", "00002", "00003"};
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(addresses[1]); // 00001
+  radio.openWritingPipe(addresses[2]); // 00002
+  radio.openWritingPipe(addresses[3]); // 00003
+  radio.setPALevel(RF24_PA_MIN);
+}
+void loop() {
+  delay(5);
+  radio.stopListening();
+  int potValue = analogRead(A0);
+  int angleValue = map(potValue, 0, 1023, 0, 180);
+  radio.write(&angleValue, sizeof(angleValue));
+     int potValue1 = analogRead(A1);
+     int angleValue1 = map(potValue1, 0, 1023, 0, 180);
+     radio.write(&angleValue1, sizeof(angleValue1));
+         int potValue2 = analogRead(A2);
+         int angleValue2 = map(potValue2, 0, 1023, 0, 180);
+         radio.write(&angleValue2, sizeof(angleValue2));
+  delay(5);
+}
