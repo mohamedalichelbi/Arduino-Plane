1d3069aa28a61242ae98b433c2d811a619dec101
mohamedalichelbi <mohamedalichelbi@icloud.com>
2017-08-14 00:30:59 +0300

    Update sketch_aug08b.ino

--- plane/RECEIVER/sketch_aug08b/sketch_aug08b.ino
+++ plane/RECEIVER/sketch_aug08b/sketch_aug08b.ino
@@ -1,18 +1,28 @@
+/*
+* Arduino Wireless Communication Tutorial
+*     Example 2 - Receiver Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <Servo.h>
+#define button 4
 RF24 radio(7, 8); // CNS, CE
-const byte addresses[][6] = {"00001", "00002", "00003"};
+const byte addresses[][6] = {"00001", "00002"};
 Servo myServo;
+Servo myServo1;
+boolean buttonState = 0;
 void setup() {
-  myServo.attach(3);
-  myServo1.attach(5);
-  myServo2.attach(6);
+  pinMode(button, INPUT);
+  myServo.attach(5);
+  myServo1.attach(6);
   radio.begin();
+  radio.openWritingPipe(addresses[0]); // 00002
   radio.openReadingPipe(1, addresses[1]); // 00001
-  radio.openReadingPipe(2, addresses[2]); // 00002
-  radio.openReadingPipe(3, addresses[3]); // 00003
   radio.setPALevel(RF24_PA_MIN);
 }
 void loop() {
@@ -23,12 +33,13 @@ void loop() {
       int angleV = 0;
       radio.read(&angleV, sizeof(angleV));
       myServo.write(angleV);
-          int angleV1 = 0;
-          radio.read(&angleV1, sizeof(angleV1));
-          myServo.write(angleV1);
-              int angleV2 = 0;
-              radio.read(&angleV2, sizeof(angleV2));
-              myServo.write(angleV2);
+         int angleV1 = 0;
+         radio.read(&angleV1, sizeof(angleV1));
+         myServo1.write(angleV1);
     }
+    delay(5);
+    radio.stopListening();
+    buttonState = digitalRead(button);
+    radio.write(&buttonState, sizeof(buttonState));
   }
 }
