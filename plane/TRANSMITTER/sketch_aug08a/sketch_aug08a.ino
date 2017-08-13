1db4b0c3637098397bd6843b07fc8aa1121a6fde
mohamedalichelbi <mohamedalichelbi@icloud.com>
2017-08-14 00:32:06 +0300

    Update sketch_aug08a.ino

--- plane/TRANSMITTER/sketch_aug08a/sketch_aug08a.ino
+++ plane/TRANSMITTER/sketch_aug08a/sketch_aug08a.ino
@@ -1,13 +1,23 @@
+/*
+* Arduino Wireless Communication Tutorial
+*     Example 2 - Transmitter Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#define led 12
 RF24 radio(7, 8); // CNS, CE
-const byte addresses[][6] = {"00001", "00002", "00003"};
+const byte addresses[][6] = {"00001", "00002"};
+boolean buttonState = 0;
 void setup() {
+  pinMode(12, OUTPUT);
   radio.begin();
   radio.openWritingPipe(addresses[1]); // 00001
-  radio.openWritingPipe(addresses[2]); // 00002
-  radio.openWritingPipe(addresses[3]); // 00003
+  radio.openReadingPipe(1, addresses[0]); // 00002
   radio.setPALevel(RF24_PA_MIN);
 }
 void loop() {
@@ -16,11 +26,17 @@ void loop() {
   int potValue = analogRead(A0);
   int angleValue = map(potValue, 0, 1023, 0, 180);
   radio.write(&angleValue, sizeof(angleValue));
-     int potValue1 = analogRead(A1);
-     int angleValue1 = map(potValue1, 0, 1023, 0, 180);
-     radio.write(&angleValue1, sizeof(angleValue1));
-         int potValue2 = analogRead(A2);
-         int angleValue2 = map(potValue2, 0, 1023, 0, 180);
-         radio.write(&angleValue2, sizeof(angleValue2));
+      int potValue1 = analogRead(A1);
+      int angleValue1 = map(potValue1, 0, 1023, 0, 180);
+      radio.write(&angleValue1, sizeof(angleValue1));
   delay(5);
+  radio.startListening();
+  while (!radio.available());
+  radio.read(&buttonState, sizeof(buttonState));
+  if (buttonState == HIGH) {
+    digitalWrite(led, HIGH);
+  }
+  else {
+    digitalWrite(led, LOW);
+  }
 }
