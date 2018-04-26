0d2ae27dbf6cf4ace0059f9270e79e79af9afac0
mohamedalichelbi <mohamedalichelbi@icloud.com>
2018-04-27 01:45:10 +0300

    Update sketch_aug08b.ino

--- plane/RECEIVER/sketch_aug08b/sketch_aug08b.ino
+++ plane/RECEIVER/sketch_aug08b/sketch_aug08b.ino
@@ -1,4 +1,5 @@
 /*
+*
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
 struct dataStruct{
@@ -7,7 +8,6 @@ struct dataStruct{
   int angleV2;
 }myData1;
 
-int value = 0; // set values you need to zero
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
@@ -18,7 +18,6 @@ const byte addresses[][6] = {"00001", "00002"};
 Servo myServo;
 Servo myServo1;
 Servo firstESC;
-boolean buttonState = 0;
 void setup() {
   pinMode(button, INPUT);
   myServo.attach(5);
@@ -28,8 +27,7 @@ void setup() {
   radio.openWritingPipe(addresses[0]); // 00002
   radio.openReadingPipe(1, addresses[1]); // 00001
       radio.setPALevel(RF24_PA_MAX);
-      radio.setDataRate(RF24_250KBPS);
-      
+      radio.setDataRate(RF24_250KBPS);     
       radio.setRetries(15,15);
       
   Serial.begin(115200);
@@ -61,7 +59,6 @@ if ( radio.available()) {
                    
              firstESC.writeMicroseconds( myData1.angleV2);
                if(Serial.available()) 
-               value = Serial.parseInt(); 
                Serial.println( myData1.angleV);
                Serial.println( myData1.angleV1);
                Serial.println( myData1.angleV2);
